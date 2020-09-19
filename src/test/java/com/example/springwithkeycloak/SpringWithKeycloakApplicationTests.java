2fa14c5a3a92194dc13cf38ce0a2d44554556c20
abdulqader <abdulqaderutm@gmail.com>
2020-09-19 11:37:09 +0800

    Initialize repository

---
+++ src/test/java/com/example/springwithkeycloak/SpringWithKeycloakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwithkeycloak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithKeycloakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
